e2068ce1b7a1a8f4e6f68f1d9a27c2aca5162550
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-23 18:41:55 -0700

    wrote pseudocode to go off of

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+//create an array of words
+//chose a word randomly
+//create underscores for size of word
+//get users guess
+//check if guess is right
+//if correct push answer to correct and choose new word
+//if wrong take away a guess left
+//keep track of letters being guessed
+//if you run out of guesses you lose
