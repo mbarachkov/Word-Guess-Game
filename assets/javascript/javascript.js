f4501a64387f68773365981aca944a8b1ef8ddea
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-24 08:23:20 -0700

    added an array with the alphabet for the letters guesssed in the word

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,13 @@
 //create an array of words
 var words = ["r2d2", "skywalker", "vader", "endor",];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //chose a word randomly
 var randChoice = Math.floor(Math.random() * words.length);
 var chosenWord = words[randChoice];
 var underScore = [];
 var pGuess = [];
+var wins = 0;
+
 console.log(chosenWord)
 //create underscores for size of word
 var makeUnderscore = () => {
@@ -25,6 +28,13 @@ document.onkeyup = function(event) {
 	console.log(pGuess[0]);
 }
 //check if guess is right
+if ((pGuess === chosenWord[0]) && (gLeft > 0)) {
+    wins++;
+    alert("Good Job!");
+    // var randChoice = words[Math.floor(Math.random() * words.length)];
+    cGuess.push(compGuess);
+    console.log(cGuess[0]);
+}
 
 //if correct push answer to correct and choose new word
 
