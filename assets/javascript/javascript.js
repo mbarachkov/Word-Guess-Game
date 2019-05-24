edc6e0735afbed856eb3348cb9717c0468b66c5f
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-23 22:13:48 -0700

    tracked the users input and console.logged it to check if working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,22 +3,27 @@ var words = ["r2d2", "skywalker", "vader", "endor",];
 //chose a word randomly
 var randChoice = Math.floor(Math.random() * words.length);
 var chosenWord = words[randChoice];
-var underScore = []
+var underScore = [];
+var pGuess = [];
 console.log(chosenWord)
 //create underscores for size of word
-var makeUnderscore = () =>   {
-    for(var i = 0; i < chosenWord.length; i++) {
+var makeUnderscore = () => {
+    for (var i = 0; i < chosenWord.length; i++) {
         underScore.push("_");
-        console.log(underScore);
+        // console.log(underScore);
     }
     return underScore;
-   
+
 }
 console.log(makeUnderscore());
 
 //get users guess
 //onkey
-
+document.onkeyup = function(event) {
+	var pGuess = event.key;
+	underScore.push(pGuess);
+	console.log(pGuess[0]);
+}
 //check if guess is right
 
 //if correct push answer to correct and choose new word
