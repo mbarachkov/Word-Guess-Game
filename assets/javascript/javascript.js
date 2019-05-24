057ce7d945003c0a705ad2da3e2b109ae056624f
Michael Barachkov <mike.barachkov@outlook.com>
2019-05-23 19:02:28 -0700

    got the randomizer picking the word to guess

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,21 @@
 //create an array of words
+var words = ["r2d2", "skywalker", "vader", "endor",];
 //chose a word randomly
+var randChoice = Math.floor(Math.random() * words.length);
+var chosenWord = words[randChoice];
+var underScore = []
+console.log(randChoice)
 //create underscores for size of word
+
 //get users guess
+//onkey
+
 //check if guess is right
+
 //if correct push answer to correct and choose new word
+
 //if wrong take away a guess left
+
 //keep track of letters being guessed
+
 //if you run out of guesses you lose
